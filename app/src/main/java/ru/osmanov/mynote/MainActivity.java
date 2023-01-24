cce0c1e696e3600b04e644e7ac76776544cde379
Nurulla Osmanov <Shivaii@yandex.ru>
2023-01-24 14:03:52 +0300

    Встроил фрагменты в активити

--- app/src/main/java/ru/osmanov/mynote/MainActivity.java
+++ app/src/main/java/ru/osmanov/mynote/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton addNote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NoteFragment noteFragment = new NoteFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container1,noteFragment).addToBackStack("").commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container2,noteFragment).addToBackStack("").commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container3,noteFragment).addToBackStack("").commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container4,noteFragment).addToBackStack("").commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container5,noteFragment).addToBackStack("").commit();
+
+        addNote = findViewById(R.id.addButton);
     }
 }
--- app/src/main/java/ru/osmanov/mynote/NotesFragment.java
+++ app/src/main/java/ru/osmanov/mynote/NotesFragment.java
@@ -2,17 +2,33 @@ package ru.osmanov.mynote;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class NotesFragment extends Fragment {
+
+    private TextView titleNotes;
+    private TextView dateNotes;
+    private TextView descriptionNotes;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_notes, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        titleNotes = view.findViewById(R.id.tv_titleNotes);
+        dateNotes = view.findViewById(R.id.tv_dateNotes);
+        descriptionNotes = view.findViewById(R.id.tv_descriptionNotes);
     }
 }
